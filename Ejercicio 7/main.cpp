96d5292120a4a0fc006bd00ecd0cb4485d46eedc
Gmongtor <gmongtor@myuax.com>
2023-11-09 13:15:51 +0100

    commit ejercicio 7 3

--- Ejercicio 7/main.cpp
+++ Ejercicio 7/main.cpp
@@ -29,3 +29,13 @@ public:
         cout << "Estado: " << estado_asistencia(estado) << endl;
     }
 };
+int main() {
+    cout << "Ingrese el nombre del estudiante: ";
+    Asistencia asistencia1("Juan", "12/05/2021", ASISTIO);
+    Asistencia asistencia2("Maria", "12/05/2021", NO_ASISTIO);
+    Asistencia asistencia3("Pedro", "12/05/2021", TARDE);
+    asistencia1.mostrar_asistencia();
+    asistencia2.mostrar_asistencia();
+    asistencia3.mostrar_asistencia();
+    return 0;
+}
