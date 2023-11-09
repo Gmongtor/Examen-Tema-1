248251b3e32882822c11be9f4e5a3dc51f7a3e62
Gmongtor <gmongtor@myuax.com>
2023-11-09 13:35:27 +0100

    Commit ejercicio 8 4

--- Ejercico 8/main.cpp
+++ Ejercico 8/main.cpp
@@ -2,32 +2,40 @@
 #include <vector>
 #include <string>
 using namespace std;
+
 class Estudiante {
 private:
     string nombre;
     int edad;
     string carrera;
+
 public:
     Estudiante(string nombre, int edad, string carrera){
         this->nombre = nombre;
         this->edad = edad;
         this->carrera = carrera;
     }
-    void mostrar_info(){
+    string getCarrera() const {
+        return carrera;
+    }
+
+    void mostrar_info() const{
         cout << "Nombre: " << nombre << endl;
         cout << "Edad: " << edad << endl;
         cout << "Carrera: " << carrera << endl;
     }
 };
+
 vector<Estudiante> filtrar_por_grados(const vector<Estudiante>& estudiantes, const string &carrera){
     vector<Estudiante> estudiantes_filtrados;
     for (const Estudiante &estudiante: estudiantes){
-        if (estudiante.carrera == carrera){
+        if (estudiante.getCarrera() == carrera){
             estudiantes_filtrados.push_back(estudiante);
         }
     }
     return estudiantes_filtrados;
 }
+
 int main(){
     vector <Estudiante> todos_estudiantes = {
             Estudiante("Juan", 20, "Ing. Sistemas"),
@@ -37,9 +45,11 @@ int main(){
             Estudiante("Ana", 19, "Ing. Sistemas"),
             Estudiante("Carlos", 21, "Ing. Sistemas"),
     };
+
     string carrera = "Ing. Sistemas";
     vector <Estudiante> estudiantes_filtrados = filtrar_por_grados(todos_estudiantes, carrera);
-cout << "Estudiantes de la carrera " << carrera << endl;
+
+    cout << "Estudiantes de la carrera " << carrera << ":" << endl;
     for (const Estudiante &estudiante: estudiantes_filtrados){
         estudiante.mostrar_info();
     }
