e2af8931dc5ab70040463147e17dca949d95dd26
Gmongtor <gmongtor@myuax.com>
2023-11-09 12:04:30 +0100

    Commit Ejercicio2 2

--- Ejercicio 2/main.cpp
+++ Ejercicio 2/main.cpp
@@ -5,10 +5,21 @@ class Estudiante {
 private:
     string nombre;
     int edad;
-    int carrera;
+    string carrera;
 public:
     Estudiante(){
         nombre = " ";
         edad = 0;
-        carrera = 0;
+        carrera = " ";
     }
+void Establecer_Datos(string nombre, int edad, string carrera){
+    this->nombre = nombre;
+    this->edad = edad;
+    this->carrera = carrera;
+}
+void Mostrar_Datos(){
+    cout << "Nombre: " << nombre << endl;
+    cout << "Edad: " << edad << endl;
+    cout << "Carrera: " << carrera << endl;
+}
+};
