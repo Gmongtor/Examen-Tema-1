13ebd15831eb17d7e6191f3fd555a10e62906200
Gmongtor <gmongtor@myuax.com>
2023-11-09 12:01:16 +0100

    Commit Ejercicio2 1

---
+++ Ejercicio 2/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class Estudiante {
+private:
+    string nombre;
+    int edad;
+    int carrera;
+public:
+    Estudiante(){
+        nombre = " ";
+        edad = 0;
+        carrera = 0;
+    }
